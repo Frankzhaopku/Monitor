84a05570fd1443bafa23a3b36d85c35a2f2530b0
Frankzhaopku <zhaoshengyu@zheli.io>
2016-09-01 15:09:08 +0800

    nothing

--- app/statsdBackend/alarm.js
+++ app/statsdBackend/alarm.js
@@ -2,39 +2,77 @@
  * Created by hzzhaoshengyu on 2016/9/1.
  */
 
+var extend = require('util')._extend;
+
 var monitorItem = {
-
   gauges: [
-    '',
-    '',
-    '',
-    '',
-    ''
+    // game server
+    'cpu.game-server-machine',
+    'mem.free.game-server-machine',
+    // udp server
+    'cpu.udp-server',
+    'rss.udp-server',
+    // redis
+    'redis.uptime.redis-server',
+    'redis.connected.clients.redis-server',
+    'redis.rejected.connections.redis-server',
+    'redis.miss.rate.redis-server'
   ],
-
   counters: [],
-
   timers: [],
+  sets: []
+};
 
-  sets: [],
+var emptyRecvData = {
+  gauges: {},
+  counters: {},
+  timers: {},
+  sets: {}
+};
 
-  timer_date: [],
-
-  statsd_metrics: []
+var alarmThreshold = {
+  
+  gauges: {
+    cpu.game-server-machine : {
+      method: 'gt',
+      change: false,
+      value: 50
+    }
+  }
 
 };
 
+var lastRecvData = null;
+
 var alarmFlush = function dataReceive (ts, metrics) {
+  var recvData = extend(emptyRecvData, {});
   for (var type in monitorItem) {
     if (monitorItem.hasOwnProperty(type)) {
+      if (!metrics.hasOwnProperty(type)) return;
       monitorItem[type].forEach(function (item) {
-        console.log(item + ":" + metrics[type][item]);
+        for (var metric in metrics[type]) {
+          if (!metrics[type].hasOwnProperty(metric)) return;
+          if (metric.indexOf(item) !== -1) {
+            recvData[type][metric] = metrics[type][metric];
+          }
+        }
       });
     }
   }
+  console.log(JSON.stringify(recvData));
+  
+  if (lastRecvData === null) {
+    lastRecvData = recvData;
+    return;
+  }
+
+  for (var type in alarmThreshold) {
+    
+  }
+
 };
 
-exports.inint = function (startupTime, config, events) {
+exports.init = function (startupTime, config, events) {
   events.on('flush', alarmFlush);
   events.on('status', function () {console.log(JSON.stringify(status));});
   return true;
